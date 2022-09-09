4da89dffdda3b49441b7cf71394cd44a73c53be7
xXOzmaXx <112040722+xXOzmaXx@users.noreply.github.com>
2022-09-09 15:59:03 -0300

    Update Importador de tabla.py

--- Importador de tabla.py
+++ Importador de tabla.py
@@ -15,6 +15,7 @@ root.title('Importador de datos')
 def select_file():
     
     filetypes = (
+        ('excel files','*.csv'),
         ('excel files','*.xlsx'),
         ('text files', '*.txt'),
         ('All files', '*.*')
@@ -45,23 +46,23 @@ def download_clicked():
     #crea boton que al apretarlo ejecute todo este script siguiente
     # Connectar a SQL Server
     conn = pyodbc.connect('Driver={SQL Server};'
-                          'Server=DESKTOP-1PSC1H3\SQLEXPRESS;'
+                          'Server=DESKTOP-JCF2VOL\SQLEXPRESS;'
                           'Database=Alumnos;'
                           'Trusted_Connection=yes;')
     cursor = conn.cursor()
     # Crea la Tabla
     cursor.execute('''
                     CREATE TABLE fichas (
-                            DNI float primary key,
+                            DNI nvarchar(255) primary key,
                             Nombre_Alumno nvarchar(255),
-                            Celular float,
+                            Celular nvarchar(255),
                 Mail nvarchar(255),
-                Fecha_de_nacimiento datetime,
+                Fecha_de_Nacimiento nvarchar(255),
                 Ciudad_de_Residencia nvarchar(255),
                 Provincia_de_Residencia nvarchar(255),
                 Pais_de_Residencia nvarchar(255),
-                Ciudad_de_nacimiento nvarchar(255),
-                Provincia_de_nacimiento nvarchar(255),
+                Ciudad_de_Nacimiento nvarchar(255),
+                Provincia_de_Nacimiento nvarchar(255),
                 Pais_de_Nacimiento nvarchar(255),
                 Estado_Civil nvarchar(255),
                 Trabaja nvarchar(255),
@@ -73,25 +74,24 @@ def download_clicked():
     # Arma un dataframe con lo que haya en el excel y lo inserta a la tabla
     for row in df.itertuples():
         cursor.execute('''
-                    INSERT INTO fichas (DNI, Nombre_Alumno, Celular, Mail, Fecha_de_nacimiento, Ciudad_de_Residencia, Provincia_de_Residencia, Pais_de_Residencia, Ciudad_de_nacimiento, Provincia_de_nacimiento, Pais_de_Nacimiento, Estado_Civil, Trabaja, Obra_Social, Nombre_Obra_Social)
-                    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
-                    ''',
-                    row.DNI, 
-                    row.Nombre_Alumno,
-                    row.Celular,
-                    row.Mail,
-                    row.Fecha_de_nacimiento,
-                    row.Ciudad_de_Residencia,
-                    row.Provincia_de_Residencia,
-                    row.Pais_de_Residencia,
-                    row.Ciudad_de_nacimiento,
-                    row.Provincia_de_nacimiento,
-                    row.Pais_de_Nacimiento,
-                    row.Estado_Civil,
-                    row.Trabaja,
-                    row.Obra_Social,
-                    row.Nombre_Obra_Social
-
+                        INSERT INTO fichas (DNI, Nombre_Alumno, Celular, Mail, Fecha_de_Nacimiento, Ciudad_de_Residencia, Provincia_de_Residencia, Pais_de_Residencia, Ciudad_de_Nacimiento, Provincia_de_Nacimiento, Pais_de_Nacimiento, Estado_Civil, Trabaja, Obra_Social, Nombre_Obra_Social)
+                        VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+                        ''',
+                        row.DNI, 
+                        row.Nombre_Alumno,
+                        row.Celular,
+                        row.Mail,
+                        row.Fecha_de_Nacimiento,
+                        row.Ciudad_de_Residencia,
+                        row.Provincia_de_Residencia,
+                        row.Pais_de_Residencia,
+                        row.Ciudad_de_Nacimiento,
+                        row.Provincia_de_Nacimiento,
+                        row.Pais_de_Nacimiento,
+                        row.Estado_Civil,
+                        row.Trabaja,
+                        row.Obra_Social,
+                        row.Nombre_Obra_Social
                     )
     conn.commit()
 
