7a33189f8de035cb9809f6dae7b4cd06646d7f6e
Gallinita10 <92393179+Gallinita10@users.noreply.github.com>
2022-09-10 06:31:42 +0200

    Update Importador de tabla.py

--- Importador de tabla.py
+++ Importador de tabla.py
@@ -1,3 +1,5 @@
+from cgi import print_form
+from pprint import pprint
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showinfo
@@ -46,55 +48,66 @@ def download_clicked():
     #crea boton que al apretarlo ejecute todo este script siguiente
     # Connectar a SQL Server
     conn = pyodbc.connect('Driver={SQL Server};'
-                          'Server=DESKTOP-JCF2VOL\SQLEXPRESS;'
+                          'Server=DESKTOP-1PSC1H3\SQLEXPRESS;'
                           'Database=Alumnos;'
                           'Trusted_Connection=yes;')
     cursor = conn.cursor()
-    # Crea la Tabla
-    cursor.execute('''
-                    CREATE TABLE fichas (
-                            DNI nvarchar(255) primary key,
-                            Nombre_Alumno nvarchar(255),
-                            Celular nvarchar(255),
-                Mail nvarchar(255),
-                Fecha_de_Nacimiento nvarchar(255),
-                Ciudad_de_Residencia nvarchar(255),
-                Provincia_de_Residencia nvarchar(255),
-                Pais_de_Residencia nvarchar(255),
-                Ciudad_de_Nacimiento nvarchar(255),
-                Provincia_de_Nacimiento nvarchar(255),
-                Pais_de_Nacimiento nvarchar(255),
-                Estado_Civil nvarchar(255),
-                Trabaja nvarchar(255),
-                Obra_Social nvarchar(255),
-                Nombre_Obra_Social nvarchar(255)
-                            )
-                '''
-            )
+    # Crea la Tabla (No es necesario porque la tabla ya va a estar creada)
+   # cursor.execute('''
+    #                CREATE TABLE fichas (
+     #                       DNI nvarchar(255) primary key,
+      #                      Nombre_Alumno nvarchar(255),
+       #                     Celular nvarchar(255),
+        #        Mail nvarchar(255),
+         #       Fecha_de_Nacimiento nvarchar(255),
+          #      Ciudad_de_Residencia nvarchar(255),
+           ##    Pais_de_Residencia nvarchar(255),
+             #   Ciudad_de_Nacimiento nvarchar(255),
+              #  Provincia_de_Nacimiento nvarchar(255),
+               # Pais_de_Nacimiento nvarchar(255),
+               # Estado_Civil nvarchar(255),
+               # Trabaja nvarchar(255),
+              #  Obra_Social nvarchar(255),
+              #  Nombre_Obra_Social nvarchar(255)
+             #               )
+            #    '''
+           # )
     # Arma un dataframe con lo que haya en el excel y lo inserta a la tabla
     for row in df.itertuples():
+        
         cursor.execute('''
-                        INSERT INTO fichas (DNI, Nombre_Alumno, Celular, Mail, Fecha_de_Nacimiento, Ciudad_de_Residencia, Provincia_de_Residencia, Pais_de_Residencia, Ciudad_de_Nacimiento, Provincia_de_Nacimiento, Pais_de_Nacimiento, Estado_Civil, Trabaja, Obra_Social, Nombre_Obra_Social)
-                        VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
+                        INSERT INTO fichas (DNI, Nombre_Alumno, Domicilio, Celular, Mail, Fecha_de_Nacimiento, Ciudad_de_Residencia, Provincia_de_Residencia, Pais_de_Residencia, Ciudad_de_Nacimiento, Provincia_de_Nacimiento, Pais_de_Nacimiento, Estado_Civil, Trabaja, Obra_Social, Nombre_Obra_Social)
+                        VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)
                         ''',
-                        row.DNI, 
-                        row.Nombre_Alumno,
-                        row.Celular,
-                        row.Mail,
-                        row.Fecha_de_Nacimiento,
-                        row.Ciudad_de_Residencia,
-                        row.Provincia_de_Residencia,
-                        row.Pais_de_Residencia,
-                        row.Ciudad_de_Nacimiento,
-                        row.Provincia_de_Nacimiento,
-                        row.Pais_de_Nacimiento,
-                        row.Estado_Civil,
-                        row.Trabaja,
-                        row.Obra_Social,
-                        row.Nombre_Obra_Social
+                        str(row.DNI), 
+                        str(row.Nombre_Alumno),
+                        str(row.Domicilio),
+                        str(row.Celular),
+                        str(row.Mail),
+                        str(row.Fecha_de_Nacimiento),
+                        str(row.Ciudad_de_Residencia),
+                        str(row.Provincia_de_Residencia),
+                        str(row.Pais_de_Residencia),
+                        str(row.Ciudad_de_Nacimiento),
+                        str(row.Provincia_de_Nacimiento),
+                        str(row.Pais_de_Nacimiento),
+                        str(row.Estado_Civil),
+                        str(row.Trabaja),
+                        str(row.Obra_Social),
+                        str(row.Nombre_Obra_Social)
                     )
-    conn.commit()
+        showinfo(
+                title='Valor Columna', 
+                message=str(row.Nombre_Obra_Social)
+            ) 
 
+    conn.commit()
+ 
+    cursor.execute('''EXECUTE('sp_reemplaza_nan')''')
+
+    conn.commit()
+    
+    conn.close()
     
     showinfo(
         title='Information',
